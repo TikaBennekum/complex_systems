0831578ac53491586c62cfd2aa2c99504facbe01
PaulJungnickel <jungnikp@gmail.com>
2025-01-23 17:01:01 +0100

    erosion working

--- src/CA.py
+++ src/CA.py
@@ -6,8 +6,10 @@ from dataclasses import dataclass
 
 ALL_NEIGHBORS = [(0, -1), (0, 1), (1, 0), (-1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]
 BOTTOM_NEIGHBORS = [(1, 0), (1, -1), (1, 1)]
+BOTTOM_SIDE_NEIGHBORS = [(1, 0), (1, -1), (1, 1),(0, -1), (0, 1)]
 
-EROSION_CONSTANT = 0.01
+EROSION_K = 0.1
+EROSION_C = 0.3
 EROSION_EXPONENT = 2.5
 
 def visualise_height(grid):
@@ -36,7 +38,7 @@ class CA:
         
     def enforce_boundary(self):        
         # Set the center cell at the top row with some water
-        self.grid[0, self.width // 2, WATER_HEIGHT] += 50  # Arbitrary water height for the top-center cell
+        self.grid[0, self.width // 2, WATER_HEIGHT] = 50  # Arbitrary water height for the top-center cell
         self.grid[-1, :, WATER_HEIGHT] = 0  # Arbitrary water height for the top-center cell
         self.grid[0, :, GROUND_HEIGHT] = self.grid[0,-1, GROUND_HEIGHT]  # Arbitrary water height for the top-center cell
         self.grid[-1, :, GROUND_HEIGHT] = 0  # Arbitrary water height for the top-center cell
@@ -62,8 +64,8 @@ class CA:
                     ni, nj = indices[idx]
                     self.grid[ni][nj][WATER_HEIGHT] += discharge
                     current_cell[WATER_HEIGHT] -= discharge
-                    current_cell[GROUND_HEIGHT] -= self.erosion_rule(EROSION_CONSTANT, discharge, slope)
-                    self.grid[ni,nj,GROUND_HEIGHT] += self.erosion_rule(EROSION_CONSTANT, discharge, slope)
+                    current_cell[GROUND_HEIGHT] -= self.erosion_rule(EROSION_K, discharge, slope)
+                    self.grid[ni,nj,GROUND_HEIGHT] += self.erosion_rule(EROSION_K, discharge, slope)
                     
 
         # If no positive slopes, distribute water evenly to zero-slope neighbors
@@ -74,8 +76,8 @@ class CA:
                 for idx in zero_slope_indices:
                     ni, nj = indices[idx]
                     self.grid[ni][nj][WATER_HEIGHT] += discharge
-                    current_cell[GROUND_HEIGHT] -=  self.erosion_rule(EROSION_CONSTANT, discharge, slope)
-                    self.grid[ni,nj,GROUND_HEIGHT] += self.erosion_rule(EROSION_CONSTANT, discharge, slope)
+                    current_cell[GROUND_HEIGHT] -=  self.erosion_rule(EROSION_K, discharge, slope)
+                    self.grid[ni,nj,GROUND_HEIGHT] += self.erosion_rule(EROSION_K, discharge, slope)
                 current_cell[WATER_HEIGHT] -= discharge * len(zero_slope_indices)
 
         # If all slopes are negative, distribute water proportionally to their magnitudes
@@ -89,14 +91,14 @@ class CA:
                         ni, nj = indices[idx]
                         self.grid[ni][nj][WATER_HEIGHT] += discharge
                         current_cell[WATER_HEIGHT] -= discharge
-                        current_cell[GROUND_HEIGHT] -= self.erosion_rule(EROSION_CONSTANT, discharge, slope)
-                        self.grid[ni,nj,GROUND_HEIGHT] += self.erosion_rule(EROSION_CONSTANT, discharge, slope)
+                        current_cell[GROUND_HEIGHT] -= self.erosion_rule(EROSION_K, discharge, slope)
+                        self.grid[ni,nj,GROUND_HEIGHT] += self.erosion_rule(EROSION_K, discharge, slope)
                         
-        
-    def erosion_rule(self, K, Q, S=0, C=0):
+           
+    def erosion_rule(self, K, Q, S=0, C=EROSION_C):
         if Q <= 0:
             return 0
-        return K* np.sign(S) * np.minimum(0.1*S, np.power(Q*(np.abs(S) + C), EROSION_EXPONENT))
+        return K* np.sign(S) * np.minimum(C, np.power(Q*(np.abs(S) + C), EROSION_EXPONENT))
 
     def create_indices_slopes(self, i: int, j: int, previous_grid: NDArray, previous_cell: NDArray) -> tuple[list[tuple[int, int]], list[float]]:
         neighbors: list[NDArray] = []
@@ -128,7 +130,7 @@ class CA:
             for j in range(self.width):
                 self.apply_rules(i, j, previous_grid)
                 
-        diff = self.grid[:,:,GROUND_HEIGHT] - previous_grid[:,:,GROUND_HEIGHT]
+        # diff = self.grid[:,:,GROUND_HEIGHT] - previous_grid[:,:,GROUND_HEIGHT]
         # print( np.max(diff), np.min(diff), np.mean(diff))
     
 
@@ -148,38 +150,41 @@ class CA:
         for _ in range(num_epochs):
             self.update_grid()
 
-            # Create a frame for the video showing water presence and height
-            frame = np.zeros((self.height, 2*self.width, 3), dtype=np.uint8)
+            if(output_file or show_live):
+                # Create a frame for the video showing water presence and height
+                frame = np.zeros((self.height, 2*self.width, 3), dtype=np.uint8)
 
-            h_range = np.max(self.grid[:,:,GROUND_HEIGHT]) - np.min(self.grid[:,:,GROUND_HEIGHT])
-            frame[:, self.width:, 2] = np.floor(255/h_range*(self.grid[:,:,GROUND_HEIGHT] - np.min(self.grid[:,:,GROUND_HEIGHT])))
-            
-            
-            h_range = np.max(self.grid[:,:,WATER_HEIGHT]) - np.min(self.grid[:,:,WATER_HEIGHT])
-            frame[:, :self.width, 0] = np.floor(155/h_range*(self.grid[:,:,WATER_HEIGHT] - np.min(self.grid[:,:,WATER_HEIGHT])))
-            
-            
-            frame[:, :self.width, 0] += (100*(self.grid[:,:,WATER_HEIGHT] > 0)).astype(np.uint8)
+                h_range = np.max(self.grid[:,:,GROUND_HEIGHT]) - np.min(self.grid[:,:,GROUND_HEIGHT])
+                frame[:, self.width:, 2] = np.floor(255/h_range*(self.grid[:,:,GROUND_HEIGHT] - np.min(self.grid[:,:,GROUND_HEIGHT])))
+                
+                # log_height = np.log10(np.maximum(1e-6, self.grid[:,:,WATER_HEIGHT]))
+                log_height = np.sqrt(np.maximum(1e-6, self.grid[:,:,WATER_HEIGHT]))
+                h_range = np.max(log_height) - np.min(log_height)
+                frame[:, :self.width, 0] = np.floor(155/h_range*(log_height - np.min(log_height)))
+                
+                
+                frame[:, :self.width, 0] += (50*(self.grid[:,:,WATER_HEIGHT] > 0)).astype(np.uint8)
+                frame[:, :self.width, 0] += (50*(self.grid[:,:,WATER_HEIGHT] > 1e-3)).astype(np.uint8)
 
 
-            # Optionally resize the frame for a larger display window
-            if show_live and window_scale != 1:
-                scaled_size = (int(frame.shape[1] * window_scale), int(frame.shape[0] * window_scale))
-                display_frame = cv2.resize(frame, scaled_size, interpolation=cv2.INTER_LINEAR)
-            else:
-                display_frame = frame
+                # Optionally resize the frame for a larger display window
+                if show_live and window_scale != 1:
+                    scaled_size = (int(frame.shape[1] * window_scale), int(frame.shape[0] * window_scale))
+                    display_frame = cv2.resize(frame, scaled_size, interpolation=cv2.INTER_LINEAR)
+                else:
+                    display_frame = frame
 
-            # Optionally show the simulation live
-            if show_live:
-                cv2.imshow("Simulation", display_frame)
-                # Wait for a short period to control the frame rate
-                # and allow the user to exit the simulation by pressing 'q'
-                if cv2.waitKey(100) & 0xFF == ord('q'):
-                    print("Simulation interrupted by user.")
-                    break
+                # Optionally show the simulation live
+                if show_live:
+                    cv2.imshow("Simulation", display_frame)
+                    # Wait for a short period to control the frame rate
+                    # and allow the user to exit the simulation by pressing 'q'
+                    if cv2.waitKey(100) & 0xFF == ord('q'):
+                        print("Simulation interrupted by user.")
+                        break
 
-            # Append the original frame for the video
-            frames.append(frame)
+                # Append the original frame for the video
+                frames.append(frame)
 
         # Close the display window
         if show_live:
