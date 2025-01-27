7d6bf00823b3d27eba036541f12fb3f66cdb37c2
PaulJungnickel <jungnikp@gmail.com>
2025-01-27 15:29:50 +0100

    stream count simulation

--- src/main-cpp.py
+++ src/main-cpp.py
@@ -7,14 +7,64 @@ from visualization2d import *
 from cpp_modules import fastCA
 
 
-if __name__ == "__main__":
-    # Example usage
+def count_num_streams(grid, threshold = 1e-9, skip_rows = 1):
+    height, width = grid.shape[:2]
+    num_streams = np.zeros(width)
+    for row in range(skip_rows, height):
+        streams = 0
+        water_cont = False
+        for col in range(width):
+            if grid[row, col, WATER_HEIGHT] > threshold:
+                if not water_cont:
+                    streams +=1
+                water_cont = True
+            else:
+                water_cont = False
+        num_streams[int(streams)] +=1
+        print(num_streams)
+    return num_streams
+
+
+def num_streams_histogram():
+    
     np.random.seed(42)
-    width, height, ground_height, num_steps = 21, 101, 101*.1, 10000
+    width, height, ground_height, num_steps = 101, 1001, 101*.1, 10000
     
     initial_state = generate_initial_slope(height, width, ground_height, noise_amplitude = 0.2, noise_type = 'white')
     
     add_central_flow(initial_state, 5)
+    
+    
+    grids = np.zeros([num_steps, height, width, NUM_CELL_FLOATS])
+    grids[0] = initial_state
+    
+    params = {
+        "EROSION_K": EROSION_K,
+        "EROSION_C": EROSION_C,
+        "EROSION_n": N,
+        "EROSION_m": EROSION_EXPONENT,
+    }
+    
+    fastCA.simulate(grids, params)
+    
+    plt.imshow(grids[-1,:,:,GROUND_HEIGHT] - initial_state[:,:,GROUND_HEIGHT])
+    plt.colorbar()
+    plt.show()
+    
+    num_streams = count_num_streams(grids[-1])
+    # plt.hist(num_streams, width)
+    plt.plot(num_streams, linestyle='', marker='o')
+    plt.savefig('data/num_stream_histogram.png')
+               
+               
+def run_and_stream():
+    
+    np.random.seed(42)
+    width, height, ground_height, num_steps = 21, 101, 101*.1, 1000
+    
+    initial_state = generate_initial_slope(height, width, ground_height, noise_amplitude = 0.2, noise_type = 'white')
+    
+    add_central_flow(initial_state, 1)
     
     
     grids = np.zeros([num_steps, height, width, NUM_CELL_FLOATS])
@@ -46,4 +96,9 @@ if __name__ == "__main__":
     plt.show()
     # plt.savefig('data/cpptest.png')
 
-    stream_video(grids)
+    stream_video(grids, scale=5) 
+
+if __name__ == "__main__":
+    # Example usage
+    num_streams_histogram()
+    # run_and_stream()
