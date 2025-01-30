afd7652b1155c865818c8a127c72dcc7aa578376
pluumenbrownie <git.wessel@beumertjes.nl>
2025-01-30 15:27:38 +0100

    tried to make caching functionality

--- src/flip_flop.py
+++ src/flip_flop.py
@@ -8,15 +8,18 @@
 """
 
 import numpy as np
+from numpy.typing import NDArray
 from cpp_modules import fastCA
 from constants import *
 from initial_state_generation import add_central_flow, generate_initial_slope
 from viewer import BarChartVisualizer
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import os
+from os.path import isfile, isdir
 
 
-EROSION_K = 0.1 # erosion rate
+EROSION_K = 0.1  # erosion rate
 EROSION_C = 0.3
 EROSION_EXPONENT = 2.5
 N = 0.5
@@ -28,11 +31,20 @@ default_params = {
     "print_params": 0,
 }
 
-def view_configuration(width, height, ground_height, num_steps, noise_amplitude, seed = 42):
+
+def view_configuration(
+    width, height, ground_height, num_steps, noise_amplitude, seed=42
+):
     np.random.seed(seed)
 
     # Generate the initial grid state
-    initial_state = generate_initial_slope(height, width, ground_height, noise_amplitude=noise_amplitude, noise_type='white')
+    initial_state = generate_initial_slope(
+        height,
+        width,
+        ground_height,
+        noise_amplitude=noise_amplitude,
+        noise_type="white",
+    )
     add_central_flow(initial_state, 1)
 
     grids = run_fastCA(initial_state, num_steps, 1_000)
@@ -41,50 +53,139 @@ def view_configuration(width, height, ground_height, num_steps, noise_amplitude,
     visualizer = BarChartVisualizer(grids)
     visualizer.run()
 
-def run_fastCA(initial_state, steps, steps_per_gen, params=default_params, update_progress = False):
+
+def cached_fastCA(
+    width,
+    height,
+    steps,
+    steps_per_gen,
+    ground_height,
+    flow_amount,
+    noise_amplitude,
+    seed,
+):
+    if not isdir(".cache"):
+        os.mkdir(".cache")
+        with open(".cache/.gitignore", "w") as file:
+            file.write("*")
+    if not isfile(".cache/index.npy"):
+        index: NDArray = np.array([])
+    else:
+        index: NDArray = np.load(".cache/index.npy")
+    state = np.array(
+        [
+            width,
+            height,
+            steps,
+            steps_per_gen,
+            ground_height,
+            flow_amount,
+            noise_amplitude,
+            seed,
+        ]
+    )
+
+    # print(f"{index = }")
+    # print(f"{index[:, :-1] = }")
+    # # print(f"{state = }")
+    # print(f"{index[:, :-1] == state = }")
+    # query = index[(index[:, :-1] == state).all()] if index.shape[0] > 0 else None
+    query = None
+    # print(f"{query = }")
+
+    assert query is None or len(query) < 2, "Cache corruption detected."
+    # print(f"{query = }")
+    query = query[0] if query is not None else None
+    # print(f"{query = }")
+
+    if query is None or len(query) == 0:
+        np.random.seed(seed)
+        initial_state = generate_initial_slope(
+            height,
+            width,
+            ground_height,
+            noise_amplitude=noise_amplitude,
+            noise_type="white",
+        )
+        add_central_flow(initial_state, flow_amount)
+
+        number = index.shape[0]
+        grids = run_fastCA(initial_state, steps, steps_per_gen)
+        save_state = np.append(state, [number]).reshape((1, 9))
+        print(f"{save_state = }")
+
+        if index.shape[0] == 0:
+            index = save_state
+        # if index.shape[0] == 1:
+        else:
+            index = np.append(index, save_state.reshape(1, 9))
+        print(f"{index = }")
+        np.save(f".cache/grids_{number}", grids)
+        np.save(".cache/index", index)
+        return grids
+
+    number = round(query[-1])
     
+    grids = np.load(f".cache/grids_{number}.npy")
+    print("Loaded grids from cache.")
+    return grids
+
+
+def run_fastCA(
+    initial_state, steps, steps_per_gen, params=default_params, update_progress=False
+):
     height, width, cell_dim = initial_state.shape
-    num_gens = steps //steps_per_gen
+    num_gens = steps // steps_per_gen
     saved_grids = np.zeros([num_gens, height, width, cell_dim])
     saved_grids[0] = initial_state
     gen_grids = np.zeros([steps_per_gen, height, width, cell_dim])
+
     for gen in tqdm(range(num_gens)):
-        if(update_progress):
-            print(gen%10, end='', flush=True)
         gen_grids[0] = saved_grids[gen]
         fastCA.simulate(gen_grids, params)
-        if gen < num_gens -1:
-            saved_grids[gen+1] = gen_grids[-1]
-        
-    if(update_progress):
-        print('')
+        if gen < num_gens - 1:
+            saved_grids[gen + 1] = gen_grids[-1]
+
     return saved_grids
+
 
 def get_stream_number(grid, threshold):
     streams_in_row = []
     for row in grid:
         stream_count = 0
-        in_stream = False 
+        in_stream = False
         for cell in row:
             if cell[WATER_HEIGHT] > threshold and not in_stream:
                 stream_count += 1
-                
+
             if cell[WATER_HEIGHT] > threshold:
                 in_stream = True
             else:
                 in_stream = False
         streams_in_row.append(stream_count)
     return np.mean(streams_in_row)
-                
 
-def stream_number_progression(width, height, ground_height, num_steps, noise_amplitude, seed):
-    np.random.seed(seed)
 
-    # Generate the initial grid state
-    initial_state = generate_initial_slope(height, width, ground_height, noise_amplitude=noise_amplitude, noise_type='white')
-    add_central_flow(initial_state, 1)
-
-    grids = run_fastCA(initial_state, num_steps, 1_000)
+def stream_number_progression(
+    width: int,
+    height: int,
+    steps: int,
+    steps_per_gen,
+    ground_height,
+    flow_amount,
+    noise_amplitude,
+    seed,
+):
+    grids = cached_fastCA(
+        width,
+        height,
+        steps,
+        steps_per_gen,
+        ground_height,
+        flow_amount,
+        noise_amplitude,
+        seed,
+    )
 
     # for threshold in [1e-6, 1e-4, 1e-2]:
     threshold = 1e-6
@@ -93,29 +194,46 @@ def stream_number_progression(width, height, ground_height, num_steps, noise_amp
         stream_list.append(get_stream_number(grid, threshold=threshold))
 
     plt.plot(np.arange(grids.shape[0]), stream_list, "-", label=f"{threshold = }")
-        
+
     plt.legend()
-    plt.savefig("videos/stream_count.png")
+    plt.savefig(f"videos/stream_count_{seed}.png")
     plt.close()
 
     derivative = abs(np.gradient(stream_list))
-    print(derivative)
     plt.plot(np.arange(len(derivative)), derivative)
-    plt.savefig("videos/stream_derivative.png")
+    plt.savefig(f"videos/stream_derivative_{seed}.png")
     plt.close()
 
-    div_hist, bins = np.histogram(derivative, 20)
+    div_hist, bins = np.histogram(
+        derivative, len(np.arange(derivative.min(), derivative.max() + 1, step=1.0))
+    )
     plt.plot(bins[:-1], div_hist, "o")
     plt.xscale("log")
     plt.yscale("log")
-    plt.savefig("videos/stream_hist.png")
+    plt.savefig(f"videos/stream_hist_{seed}.png")
     plt.close()
 
     visualizer = BarChartVisualizer(grids)
     visualizer.run()
 
 
-width, height, ground_height, num_steps = 21, 101, 51 * 0.1, 200_000
+width = 21
+height = 101
+steps = 20_000
+steps_per_gen = 100
+ground_height = 51 * 0.1
+flow_amount = 1.0
 noise_amplitude = 0.2
+seed = 42
 
-stream_number_progression(width, height, ground_height, num_steps, noise_amplitude, seed=42)
+cached_fastCA(
+    width,
+    height,
+    steps,
+    steps_per_gen,
+    ground_height,
+    flow_amount,
+    noise_amplitude,
+    seed,
+)
+# cached_fastCA()
